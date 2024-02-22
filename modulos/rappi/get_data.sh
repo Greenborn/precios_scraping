197ecfcbcd99b2331cc43a41f404b65f55e19a78
Luciano <lucho.2012.tandil@gmail.com>
2024-02-22 15:53:12 -0300

    Se quita origen de datos de rappi

--- modulos/rappi/get_data.sh
+++ modulos/rappi/get_data.sh
@@ -26,24 +26,6 @@ python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212002-carre
 python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212002-carrefour --categoria 1
 python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212002-carrefour --categoria 0
 
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 0
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 1
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 2
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 3
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 4
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 5
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 6
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 7
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 8
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 9
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 10
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 11
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 12
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 13
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 14
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 15
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 16
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/210686-simplicity-market --categoria 17
 
 python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 25
 python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 24
