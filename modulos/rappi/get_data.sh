4d890af2b395cba11b2c292aff5d9706d0ceb5db
Luciano <lucho.2012.tandil@gmail.com>
2024-02-24 17:31:52 -0300

    Se quita vea de origen de datos anterior

--- modulos/rappi/get_data.sh
+++ modulos/rappi/get_data.sh
@@ -26,34 +26,6 @@ python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212002-carre
 python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212002-carrefour --categoria 1
 python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212002-carrefour --categoria 0
 
-
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 25
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 24
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 23
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 22
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 21
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 20
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 19
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 18
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 17
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 16
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 15
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 14
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 13
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 12
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 11
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 10
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 9
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 8
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 7
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 6
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 5
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 4
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 3
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 2
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 1
-python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/212819-vea --categoria 0
-
 python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/214038-los3pollitos-mt-nc --categoria 1
 python3 get_data_cat.py --comercio https://www.rappi.com.ar/tiendas/214038-los3pollitos-mt-nc --categoria 0
 
