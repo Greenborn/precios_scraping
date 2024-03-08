601dd8f500b0e581f63eb3e47f527ead0aa5fec5
Luciano <lucho.2012.tandil@gmail.com>
2024-03-08 16:40:12 -0300

    Origen de datos finalizado

--- modulos/chasqui/get_data.py
+++ modulos/chasqui/get_data.py
@@ -84,28 +84,68 @@ for categoria in categorias:
         driver.get(url)
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
 
-        res_mas = True
-        while res_mas:
-            scroll_hasta_el_final(driver)
-            res_mas = hacer_clic_por_texto(driver, 'Cargar más productos')
-
         res_consulta = driver.page_source
-        soup = BeautifulSoup(res_consulta, 'html.parser')
-        data_ = soup.find_all(class_="product-card")
+        auth_token = driver.execute_script("return sessionStorage.getItem('authToken')")
         
-        for prod in data_:
+        headers_ = {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/123.0",
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Language": "es-AR,es;q=0.8,en-US;q=0.5,en;q=0.3",
+            "X-Requested-With": "XMLHttpRequest",
+            "Origin": "https://tiendaschasqui.ar",
+            "vendure-token": "csd",
+            "authorization": "Bearer " + auth_token,
+            "Connection": "keep-alive",
+            "Referer": url
+        }
+
+        url_api = "https://tiendaschasqui.ar/shop-api"
+        cat_slug = url.split("/")
+        cat_slug = cat_slug[len(cat_slug)-1]
+        response_cat_d = requests.post(url_api, headers=headers_, json=
+            {
+                "operationName":"GetCollection",
+                "variables":{"slug":cat_slug},
+                "query":"query GetCollection($id: ID, $slug: String) {\n  collection(id: $id, slug: $slug) {\n    id\n    name\n    slug\n    description\n    featuredAsset {\n      ...Asset\n      __typename\n    }\n    breadcrumbs {\n      id\n      slug\n      name\n      __typename\n    }\n    children {\n      id\n      slug\n      featuredAsset {\n        ...Asset\n        __typename\n      }\n      name\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment Asset on Asset {\n  id\n  width\n  height\n  name\n  preview\n  focalPoint {\n    x\n    y\n    __typename\n  }\n  __typename\n}\n"
+            })
+        
+        response_cat_d = response_cat_d.json()
+        id_colection = response_cat_d['data']['collection']['id']
+        
+        response = requests.post(url_api, headers=headers_, json = 
+            {
+                "operationName":"SearchProducts",
+                "variables":{
+                    "input":{
+                        "term":"","groupByProduct":True,
+                        "collectionId":id_colection,
+                        "facetValueFilters":[],
+                        "take":1200,"skip":0,"sort":{"price":"ASC"}
+                    }
+                },
+                "query":"query SearchProducts($input: SearchInput!) {\n  search(input: $input) {\n    items {\n      productVariantId\n      productId\n      slug\n      productName\n      description\n      facetValueIds\n      inStock\n      priceWithTax {\n        ... on PriceRange {\n          min\n          max\n          __typename\n        }\n        __typename\n      }\n      productAsset {\n        id\n        preview\n        focalPoint {\n          x\n          y\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    totalItems\n    facetValues {\n      count\n      facetValue {\n        id\n        name\n        code\n        facet {\n          id\n          name\n          code\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"
+            })
+        data_items = response.json()
+        data_items = data_items["data"]["search"]["items"]
+        
+        for prod in data_items:
             try:
-                name = prod.find(class_="product-details")
-                name = name.find(class_="product-details__name").text.strip()
-
-                price = prod.find(class_="product-details__price").text.replace(" ","").replace("$","").replace(".","").replace(",",".")
-
+                headers_["Referer"] = "https://tiendaschasqui.ar/csd/producto/"+prod["slug"]
+                response_prod = requests.post(url_api, headers=headers_, json = 
+                    {
+                        "operationName":"GetProductDetail",
+                        "variables":{"slug":prod["slug"]},
+                        "query":"query GetProductDetail($slug: String!) {\n  product(slug: $slug) {\n    id\n    name\n    description\n    slug\n    variants {\n      id\n      name\n      options {\n        code\n        name\n        __typename\n      }\n      price\n      priceWithTax\n      sku\n      stockLevel\n      __typename\n    }\n    featuredAsset {\n      ...Asset\n      __typename\n    }\n    assets {\n      ...Asset\n      __typename\n    }\n    collections {\n      id\n      slug\n      breadcrumbs {\n        id\n        name\n        slug\n        __typename\n      }\n      __typename\n    }\n    facetValues {\n      name\n      code\n      __typename\n    }\n    customFields {\n      productor {\n        id\n        name\n        localidad\n        provincia\n        descriptionOffered\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment Asset on Asset {\n  id\n  width\n  height\n  name\n  preview\n  focalPoint {\n    x\n    y\n    __typename\n  }\n  __typename\n}\n"
+                    })
+                data_response_prod = response_prod.json()
+                data_prod = data_response_prod["data"]["product"]
                 producto = {
                         "vendor_id": 58,
-                        "name": categoria + " - " + name,
-                        "price": float(price),
-                        "url": "https://tiendaschasqui.ar/csd/",
+                        "name": categoria + " - " + data_prod["name"],
+                        "price": float(prod["priceWithTax"]["max"])/100,
+                        "url": "https://tiendaschasqui.ar/csd/producto/"+data_prod["slug"],
                         "is_ext": "",
+                        "nota": "Productor: " + data_prod["customFields"]["productor"]["name"], 
                         "branch_id": BRANCH_ID,
                         "category": categorias[categoria]["category"],
                         "key": config["BACK_KEY"]
