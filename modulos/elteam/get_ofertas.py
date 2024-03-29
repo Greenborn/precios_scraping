d0cb4d7a30edac42df1c3ebb2d2f15d5f4298baa
Luciano <lucho.2012.tandil@gmail.com>
2024-03-29 20:14:58 -0300

    soporte de ofertas para el team computación

--- modulos/elteam/get_ofertas.py
+++ modulos/elteam/get_ofertas.py
@@ -12,9 +12,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 BRANCH_ID = 88
-
-with open('categorias.json') as archivo_json:
-    categorias = json.load(archivo_json)
+URL_OFERTAS = "https://eltheamcomputacion.mitiendanube.com/outlet/"
 
 with open("../config.json", "r") as archivo:
     config = json.load(archivo)
@@ -41,33 +39,32 @@ def procesar_resultados(res_consulta):
                 print("Sin Stock")
                 continue
 
-        producto = {
-                    "vendor_id": 58,
-                    "name": data_["name"] + " - " + data_["description"],
-                    "price": float(data_["offers"]["price"]),
-                    "is_ext": "",
-                    "url": data_["offers"]["url"],
-                    "branch_id": BRANCH_ID,
-                    "all_data": data_,
-                    "category": categorias[categoria]["category"],
-                    "key": config["BACK_KEY"]
+        promocion = {
+            "orden":       0,
+            "titulo":      data_["name"] + " - " + data_["description"],
+            "id_producto": 0,
+            #"datos_extra": { "promo_cnt": promo_cnt },
+            "datos_extra": {},
+            "precio":      float(data_["offers"]["price"]),
+            "branch_id":   BRANCH_ID,
+            "url":         data_["offers"]["url"],
+            "key":         config["BACK_KEY"]
         }
 
-        enviar_back = requests.post(config["URL_BACK"] + "/publico/productos/importar", json=producto)
+        enviar_back = requests.post(config["URL_BACK"] + "/publico/productos/importar_oferta", json=promocion)
         print(enviar_back.json())
-        listado_productos.append(producto)
-        print(producto)
+        listado_productos.append(promocion)
+        print(promocion)
+        print("")
 
 def scroll_hasta_el_final(driver):
     last_scroll_position = 0
     while True:
-        # Mover el scroll hasta el final de la página actual
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         
         time.sleep(2)
         current_scroll_position = driver.execute_script("return window.pageYOffset")
 
-        # Si no hay más contenido para mostrar (es decir, no se ha desplazado más), salir del bucle
         if current_scroll_position == last_scroll_position:
             break
 
@@ -79,22 +76,19 @@ options.add_argument('--disable-dev-shm-usage')
 #options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 
-for categoria in categorias:
-    print("Procesado categoria: ",categoria)
+url = URL_OFERTAS
+print("haciendo petición a: ", url)
+driver.get(url)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
+res_consulta = driver.page_source
 
-    url = categorias[categoria]['url']
-    print("haciendo petición a: ", url)
-    driver.get(url)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
-    res_consulta = driver.page_source
-    
-    scroll_hasta_el_final(driver)
-    procesar_resultados(res_consulta)
-    
+scroll_hasta_el_final(driver)
+procesar_resultados(res_consulta)
 
-    path = 'salida/productos_cat'+fecha+'.json'
-    with open(path, 'w') as file:
-        json.dump(listado_productos, file)
-        print(path,' actualizado')
+
+path = 'salida/productos_ofertas_'+fecha+'.json'
+with open(path, 'w') as file:
+    json.dump(listado_productos, file)
+    print(path,' actualizado')
 
 print("obtenidos ", len(listado_productos))
