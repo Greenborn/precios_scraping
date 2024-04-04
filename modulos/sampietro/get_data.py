e343f33d0f5f3c17561d943051a36340c8bf1f35
Luciano <lucho.2012.tandil@gmail.com>
2024-04-04 17:41:15 -0300

    se agrega parametro de categoria inicial

--- modulos/sampietro/get_data.py
+++ modulos/sampietro/get_data.py
@@ -4,6 +4,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import argparse
 
 with open('categorias.json') as archivo_json:
     categorias = json.load(archivo_json)
@@ -20,61 +21,83 @@ listado_productos = []
 
 diccio_nam = {}
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--categoria_inicio", type=str, help="Categoria desde la cual se procesan resultados")
+args = parser.parse_args()
+categoria_inicio = args.categoria_inicio
+
+procesar = True
+print(categoria_inicio)
+
+if (categoria_inicio != None):
+    procesar = False
+
 for categoria in categorias:
-    id = categorias[categoria]['id']
-    url = "https://sampietroweb.com.ar/Item/Search/?page=1&id="+str(id)+"&recsPerPage=2400&order=id&sort=False&itemtype=Category%2C%20Content%2C%20Product&term=&getFilterData=True&filters=&fields=Name"
-    response = requests.get(url)
 
-    response = response.text
-    soup = BeautifulSoup(response, 'html.parser')
+    if (categoria == categoria_inicio):
+        print(categoria, categoria_inicio)
+        procesar = True
+        continue
+    
+    if (procesar == True):
+        id = categorias[categoria]['id']
+        url = "https://sampietroweb.com.ar/Item/Search/?page=1&id="+str(id)+"&recsPerPage=2400&order=id&sort=False&itemtype=Category%2C%20Content%2C%20Product&term=&getFilterData=True&filters=&fields=Name"
+        response = requests.get(url)
 
-    articulos = soup.find_all("article")
+        response = response.text
+        soup = BeautifulSoup(response, 'html.parser')
 
-    for articulo in articulos:
-        enlace_cnt = articulo.find(class_="box_image")
-        enlace     = enlace_cnt.find("a")
+        articulos = soup.find_all("article")
 
-        nombre_cnt = articulo.find(class_="box_data")
-        nombre     = nombre_cnt.find("h3").text.strip()
+        for articulo in articulos:
+            enlace_cnt = articulo.find(class_="box_image")
+            enlace     = enlace_cnt.find("a")
 
-        if (nombre in diccio_nam):
-            print("producto repetido", nombre)
-            continue
+            nombre_cnt = articulo.find(class_="box_data")
+            nombre     = nombre_cnt.find("h3").text.strip()
 
-        diccio_nam[nombre] = 1
+            if (nombre in diccio_nam):
+                print("producto repetido", nombre)
+                continue
 
-        precio = articulo.find(class_="price_wrapper")
-        if (precio is None):
-            print("No se encuentra precio")
-            continue
-        
-        precio = precio.find(class_="precio-final")
-        if (precio is None):
-            print("No se encuentra precio, no disponible?")
-            continue
+            diccio_nam[nombre] = 1
 
-        precio = precio.text.replace(".", "").replace(",", ".").strip()
+            precio = articulo.find(class_="price_wrapper")
+            if (precio is None):
+                print("No se encuentra precio")
+                continue
+            
+            precio = precio.find(class_="precio-final")
+            if (precio is None):
+                print("No se encuentra precio, no disponible?")
+                continue
 
-        producto = {
-            "vendor_id": 58,
-            "name": categoria + " - " + nombre,
-            "url": BASE_URL + enlace.get("href"),
-            "price": float(precio),
-            "is_ext": "",
-            "branch_id": BRANCH_ID,
-            "category": categorias[categoria]["category"],
-            "key": config["BACK_KEY"]
-        }
+            precio = precio.text.replace(".", "").replace(",", ".").strip()
 
-        enviar_back = requests.post(config["URL_BACK"] + "/publico/productos/importar", json=producto)
-        print(enviar_back.json())
-        listado_productos.append(producto)
+            producto = {
+                "vendor_id": 58,
+                "name": categoria + " - " + nombre,
+                "url": BASE_URL + enlace.get("href"),
+                "price": float(precio),
+                "is_ext": "",
+                "branch_id": BRANCH_ID,
+                "category": categorias[categoria]["category"],
+                "key": config["BACK_KEY"]
+            }
 
-        print(producto)
-        print("")
+            enviar_back = requests.post(config["URL_BACK"] + "/publico/productos/importar", json=producto)
+            print(enviar_back.json())
+            listado_productos.append(producto)
 
-    path = 'salida/productos_cat'+fecha+'.json'
-    with open(path, 'w') as file:
-        json.dump(listado_productos, file)
-        print(path,' actualizado')
+            print(producto)
+            print("")
 
+        path = 'salida/productos_cat'+fecha+'.json'
+        with open(path, 'w') as file:
+            json.dump(listado_productos, file)
+            print(path,' actualizado')
+    else:
+        print("ignorando categoria: ", categoria)
+        continue
+print("cantidad de elementos: ", len(listado_productos))
