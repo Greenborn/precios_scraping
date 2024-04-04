3f6943ce78b1d3700d7c4a974039a4355ffba9e5
Luciano <lucho.2012.tandil@gmail.com>
2024-04-04 17:37:32 -0300

    Se actuializa obtencion de ofertas se agrega parametro de categoria inicial

--- modulos/otero/get_data.py
+++ modulos/otero/get_data.py
@@ -10,6 +10,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import argparse
 
 BRANCH_ID = 93
 URL_BASE = "https://www.otero.com.ar"
@@ -21,6 +22,12 @@ with open("../config.json", "r") as archivo:
     config = json.load(archivo)
 
 fecha = datetime.datetime.now().strftime("%Y%m%d")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--categoria_inicio", type=str, help="Categoria desde la cual se procesan resultados")
+args = parser.parse_args()
+categoria_inicio = args.categoria_inicio
 
 listado_productos = []
 
@@ -86,41 +93,57 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 
+procesar = True
+print(categoria_inicio)
+
+if (categoria_inicio != None):
+    procesar = False
+
 for categoria in categorias:
     print("Procesado categoria: ",categoria)
 
-    url = URL_BASE+categorias[categoria]['url']
-    print("haciendo petición a: ", url)
-    driver.get(url)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
-    res_consulta = driver.page_source
+    if (categoria == categoria_inicio):
+        print(categoria, categoria_inicio)
+        procesar = True
+        continue
     
-    scroll_hasta_el_final(driver)
-
-    soup2 = BeautifulSoup(res_consulta, 'html.parser')
-    paginacion = soup2.find(class_="pagination_wrapper")
-
-    if (paginacion == None):
-        print("No hay paginacion")
-        procesar_resultados(res_consulta, categoria)
-        continue
-    paginas = paginacion.find_all("a")
-    if len(paginas) == 0:
-        print("No hay paginacion")
-        procesar_resultados(res_consulta, categoria)
-        continue
-
-    for pagina in paginas:
-        print(pagina.get("href"))
-        driver.get(url+pagina.get("href"))
+    if (procesar == True):
+        url = URL_BASE+categorias[categoria]['url']
+        print("haciendo petición a: ", url)
+        driver.get(url)
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
         res_consulta = driver.page_source
         
         scroll_hasta_el_final(driver)
-        procesar_resultados(res_consulta, categoria)
-    
 
-    path = 'salida/productos_cat'+fecha+'.json'
-    with open(path, 'w') as file:
-        json.dump(listado_productos, file)
-        print(path,' actualizado')
+        soup2 = BeautifulSoup(res_consulta, 'html.parser')
+        paginacion = soup2.find(class_="pagination_wrapper")
+
+        if (paginacion == None):
+            print("No hay paginacion")
+            procesar_resultados(res_consulta, categoria)
+            continue
+        paginas = paginacion.find_all("a")
+        if len(paginas) == 0:
+            print("No hay paginacion")
+            procesar_resultados(res_consulta, categoria)
+            continue
+
+        for pagina in paginas:
+            print(pagina.get("href"))
+            driver.get(url+pagina.get("href"))
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
+            res_consulta = driver.page_source
+            
+            scroll_hasta_el_final(driver)
+            procesar_resultados(res_consulta, categoria)
+        
+
+        path = 'salida/productos_cat'+fecha+'.json'
+        with open(path, 'w') as file:
+            json.dump(listado_productos, file)
+            print(path,' actualizado')
+    else:
+        print("ignorando categoria: ", categoria)
+        continue
+print("cantidad de elementos: ", len(listado_productos))
