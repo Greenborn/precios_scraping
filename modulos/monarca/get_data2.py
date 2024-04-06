aa27fb9db49848692914fdf06f3b5cfe5efa2c86
Luciano <lucho.2012.tandil@gmail.com>
2024-04-06 17:47:51 -0300

    se usa envio centralizado de informacion

--- modulos/monarca/get_data2.py
+++ modulos/monarca/get_data2.py
@@ -4,6 +4,7 @@ import datetime
 import json
 import requests
 import argparse
+import socketio
 
 URL_CATEGORIAS = "http://api.monarcadigital.com.ar/categories/struct?version=87ddd4f1-8d9b-4cbf-a677-07db09562e7c"
 
@@ -48,94 +49,111 @@ def convertir_fecha(fecha_str):
     fecha_datetime = datetime.datetime.strptime(fecha_str, "%d/%m/%Y %H:%M:%S")
     return fecha_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
-for categoria in matchs:
-    url_consulta_cat = "http://api.monarcadigital.com.ar/categories/"+categoria+"/products" 
-    print("Consultando URL: ", url_consulta_cat )
+with socketio.SimpleClient() as sio:
+    sio.connect('http://localhost:7777')
 
-    if (categoria == categoria_inicio):
-        print(categoria, categoria_inicio)
-        procesar = True
-        continue
-    
-    if (procesar == True):
-        response = requests.get(url_consulta_cat)
+    sio.emit('cliente_conectado')
+    if (not sio.receive()[1]["status"]):
+        print("Rechazado")
+        exit()
 
-        if not response.json():
-            print('Se obtuvo respuesta vacía')
+    for categoria in matchs:
+        url_consulta_cat = "http://api.monarcadigital.com.ar/categories/"+categoria+"/products" 
+        print("Consultando URL: ", url_consulta_cat )
+
+        if (categoria == categoria_inicio):
+            print(categoria, categoria_inicio)
+            procesar = True
             continue
+        
+        if (procesar == True):
+            response = requests.get(url_consulta_cat)
 
-        response = response.json()
-        for registro in response:
-            
-            if registro['status'] != "Publicado":
-                print("No publicado")
+            if not response.json():
+                print('Se obtuvo respuesta vacía')
                 continue
 
-            presentacion = ""
-            if registro['presentation'] != None:
-                presentacion = ' - ' + registro['presentation'].strip()
+            response = response.json()
+            for registro in response:
+                
+                if registro['status'] != "Publicado":
+                    print("No publicado")
+                    continue
 
-            nomobre_completo_prod = registro['description'].strip() + ' - ' + registro['brand'].strip() + ' - ' + presentacion
-            oferta_data           = registro['promotions']
-            categoria_asignada    = matchs[str(registro['category']['id'])]
+                presentacion = ""
+                if registro['presentation'] != None:
+                    presentacion = ' - ' + registro['presentation'].strip()
 
-            if (len(oferta_data) == 1):
-                print(oferta_data, len(oferta_data))
-                print("")
-                oferta_ = oferta_data[0]
-                if (oferta_["type"] in tipos_ofertas):
-                    texto_descuento = oferta_["content"]
-                    promocion = {
-                                    "orden":       0,
-                                    "titulo":      texto_descuento + ' - ' + nomobre_completo_prod,
-                                    "id_producto": 0,
-                                    "datos_extra": { "promo_cnt": texto_descuento, 
-                                                     "_data": oferta_, 
-                                                     "hasta": convertir_fecha(oferta_["dateTo"]),
-                                                     "desde": convertir_fecha(oferta_["fromDate"]),
-                                                    },
-                                    "precio":      float(registro["price"]),
-                                    "branch_id":   BRANCH,
-                                    "url":         URL_MONARCA_APP,
-                                    "key":         config["BACK_KEY"]
-                                }
-                    print(promocion)
-                    enviar_back = requests.post(config["URL_BACK"] + "/publico/productos/importar_oferta", json=promocion)
-                    todos_los_descuentos.append(promocion)
-                    print(enviar_back.json())
-                else:
+                nomobre_completo_prod = registro['description'].strip() + ' - ' + registro['brand'].strip() + ' - ' + presentacion
+                oferta_data           = registro['promotions']
+                categoria_asignada    = matchs[str(registro['category']['id'])]
+
+                if (len(oferta_data) == 1):
+                    print(oferta_data, len(oferta_data))
+                    print("")
+                    oferta_ = oferta_data[0]
+                    if (oferta_["type"] in tipos_ofertas):
+                        try:
+                            texto_descuento = oferta_["content"]
+                            promocion = {
+                                            "orden":       0,
+                                            "titulo":      texto_descuento + ' - ' + nomobre_completo_prod,
+                                            "id_producto": 0,
+                                            "datos_extra": { "promo_cnt": texto_descuento, 
+                                                            "_data": oferta_, 
+                                                            "hasta": convertir_fecha(oferta_["dateTo"]),
+                                                            "desde": convertir_fecha(oferta_["fromDate"]),
+                                                            },
+                                            "precio":      float(registro["price"]),
+                                            "branch_id":   BRANCH,
+                                            "url":         URL_MONARCA_APP,
+                                            "key":         config["BACK_KEY"]
+                                        }
+                            print(promocion)
+                            
+                            sio.emit('registrar_oferta', promocion)
+                            todos_los_descuentos.append(promocion)
+                            print("")
+                        except:
+                            print("no se pudo procesar oferta")
+                            ofertas_no_procesadas.append(oferta_)
+                            print(oferta_)
+                            with open('ofertas_no_procesadas.json', 'w') as file:
+                                json.dump(ofertas_no_procesadas, file)
+                            continue
+                    else:
+                        ofertas_no_procesadas.append(registro)
+                        print("no se procesa oferta")
+                        with open('ofertas_no_procesadas.json', 'w') as file:
+                            json.dump(ofertas_no_procesadas, file)
+                    
+                elif (len(oferta_data) > 1):
                     ofertas_no_procesadas.append(registro)
-                    print("no se procesa oferta")
+                    print("hay mas de una oferta")
                     with open('ofertas_no_procesadas.json', 'w') as file:
                         json.dump(ofertas_no_procesadas, file)
-                
-            elif (len(oferta_data) > 1):
-                ofertas_no_procesadas.append(registro)
-                print("hay mas de una oferta")
-                with open('ofertas_no_procesadas.json', 'w') as file:
-                    json.dump(ofertas_no_procesadas, file)
-            elif (len(oferta_data) == 0):
-                producto = { 
-                    "name": nomobre_completo_prod,
-                    "price": float(registro['price']),
-                    "vendor_id": VENDOR,
-                    "branch_id": BRANCH,
-                    #"reg": registro,
-                    "url": URL_MONARCA_APP,
-                    "category": categoria_asignada,
-                    "key": config["BACK_KEY"]
-                }
-                print(producto)
-                enviar_back = requests.post(config["URL_BACK"] + "/publico/productos/importar", json=producto)
-                print(enviar_back.json())
-                productos.append(producto)
+                elif (len(oferta_data) == 0):
+                    producto = { 
+                        "name": nomobre_completo_prod,
+                        "price": float(registro['price']),
+                        "vendor_id": VENDOR,
+                        "branch_id": BRANCH,
+                        #"reg": registro,
+                        "url": URL_MONARCA_APP,
+                        "category": categoria_asignada,
+                        "key": config["BACK_KEY"]
+                    }
+                    print(producto)
+                    sio.emit('registrar_precio', producto)
+                    print("")
+                    productos.append(producto)
 
-        with open(path, 'w') as file:
-            json.dump(productos, file)
-    else:
-        print("ignorando categoria: ", categoria)
-        continue
+            with open(path, 'w') as file:
+                json.dump(productos, file)
+        else:
+            print("ignorando categoria: ", categoria)
+            continue
 
-print("Productos a agregar: ", len(productos))
-print("promociones", len(todos_los_descuentos))
-print("Productos no publicados: ", len(productos_no_publicados))
+    print("Productos a agregar: ", len(productos))
+    print("promociones", len(todos_los_descuentos))
+    print("Productos no publicados: ", len(productos_no_publicados))
