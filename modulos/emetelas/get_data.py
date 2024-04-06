82349ed276d7979481bbaa50f4b4520c25f494e6
Luciano <lucho.2012.tandil@gmail.com>
2024-04-06 17:48:09 -0300

    Se usa envio centralizado de informacion

--- modulos/emetelas/get_data.py
+++ modulos/emetelas/get_data.py
@@ -3,6 +3,7 @@
 import requests
 import datetime
 import json
+import socketio
 
 BRANCH_ID = 83
 fecha = datetime.datetime.now().strftime("%Y%m%d")
@@ -35,33 +36,44 @@ categorias = [
 
 listado_productos = []
 
-for categoria in categorias:
-    url = categoria["url"]
-    categoria = categoria["category"]
-    response = requests.get(url)
 
-    if response.json():
-        respuesta = response.json()
-        respuesta = respuesta["data"]
+with socketio.SimpleClient() as sio:
+    sio.connect('http://localhost:7777')
 
-        for prod in respuesta:
-            if prod["status"] == "available":
-                producto = {
-                    "vendor_id": 58,
-                    "name": prod["name"],
-                    "price": prod["price"],
-                    "is_ext": prod["id"],
-                    "branch_id": BRANCH_ID,
-                    "category": categoria,
-                    "url": "https://emetelas.com.ar/"+prod["slug"],
-                    "key": config["BACK_KEY"]
-                }
-                enviar_back = requests.post(config["URL_BACK"] + "/publico/productos/importar", json=producto)
-                print(enviar_back.json())
-                listado_productos.append(producto)
-                print(producto)
+    sio.emit('cliente_conectado')
+    if (not sio.receive()[1]["status"]):
+        print("Rechazado")
+        exit()
 
-path = 'salida/productos_cat'+fecha+'.json'
-with open(path, 'w') as file:
-    json.dump(listado_productos, file)
-    print('estado.json actualizado')
+    for categoria in categorias:
+        url = categoria["url"]
+        categoria = categoria["category"]
+        response = requests.get(url)
+
+        if response.json():
+            respuesta = response.json()
+            respuesta = respuesta["data"]
+
+            for prod in respuesta:
+                if prod["status"] == "available":
+                    producto = {
+                        "vendor_id": 58,
+                        "name": prod["name"],
+                        "price": prod["price"],
+                        "is_ext": prod["id"],
+                        "branch_id": BRANCH_ID,
+                        "category": categoria,
+                        "url": "https://emetelas.com.ar/"+prod["slug"],
+                        "key": config["BACK_KEY"]
+                    }
+                    sio.emit('registrar_precio', producto)
+                    listado_productos.append(producto)
+                    print(producto)
+                    print("")
+
+    path = 'salida/productos_cat'+fecha+'.json'
+    with open(path, 'w') as file:
+        json.dump(listado_productos, file)
+        print('estado.json actualizado')
+
+    print("obtenidos ", len(listado_productos))
