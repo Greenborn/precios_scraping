cdaecda0343dd16ea896bbc505c47fbbd0ebb87b
Luciano <lucho.2012.tandil@gmail.com>
2024-04-06 19:31:38 -0300

    Se mejora el servidor para desacoplar el envio de información de la recepción de información, entonces todo lo que llega se agrega a una cola, la cual se va procesando a intervalos regulares

--- server.js
+++ server.js
@@ -20,6 +20,21 @@ let errores_envios = {
     }
 }
 
+let envios_server = {
+    'registrar_precio': {
+        "data_enviar":[],
+        "data_enviada": [],
+        "data_error": [],
+        "url": process.env.URL_BACK  + "/publico/productos/importar"
+    },
+    'registrar_oferta': {
+        "data_enviar":[],
+        "data_enviada": [],
+        "data_error": [],
+        "url": process.env.URL_BACK  + "/publico/productos/importar_oferta"
+    }
+}
+
 io.on('connection', socket => {
   console.log(`connect ${socket.id}`);
 
@@ -32,37 +47,52 @@ io.on('connection', socket => {
 
   socket.on('registrar_precio', ( data ) => {
     console.log('registrar_precio')
-
-    axios.post(process.env.URL_BACK  + "/publico/productos/importar" , data)
-        .then(function (response) {
-            console.log(response.data);
-            socket.emit('registrar_precio', response.data);
-        })
-        .catch(function (error) {
-            let error_r = errores_envios['fallo_conexion']['registrar_precio']
-            error_r.push({ "data": data, "error": error });
-            console.log("Error al realizar petición, errores hasta el momento: ", error_r.length );
-        })
+    envios_server['registrar_precio'].data_enviar.push(data)
   });
 
   socket.on('registrar_oferta', ( data ) => {
     console.log('registrar_oferta')
-
-    axios.post(process.env.URL_BACK  + "/publico/productos/importar_oferta" , data)
-        .then(function (response) {
-            console.log(response.data);
-            socket.emit('registrar_oferta', response.data);
-        })
-        .catch(function (error) {
-            let error_r = errores_envios['fallo_conexion']['registrar_oferta']
-            error_r.push({ "data": data, "error": error });
-            console.log("Error al realizar petición, errores hasta el momento: ", error_r.length );
-        })
+    envios_server['registrar_oferta'].data_enviar.push(data)
   });
 
   socket.on('disconnect', () => {
     console.log(`disconnect ${socket.id}`);
   });
 });
+
+const INTERVALO_ENVIO = 100
+const RAFAGAS_ENVIO = 1
+
+async function procesar_envios() {
+    let tipos_envios = Object.keys(envios_server)
+    for (let i = 0; i < tipos_envios.length; i++) {
+        for (let j = 0; j < RAFAGAS_ENVIO; j++) {
+            let envio_info  = envios_server[tipos_envios[i]]
+            let lista_envio = envio_info.data_enviar
+            let url_envio   = envio_info.url
+            let cantidad = lista_envio.length
+            if (cantidad > 0) {
+                let elemento = lista_envio.pop()
+                console.log( tipos_envios[i],' Por enviar ', cantidad, ' enviadas ', envio_info.data_enviada.length, ' errores ', envio_info.data_error.length)
+                axios.post(url_envio, elemento)
+                    .then(function (response) {
+                    //console.log(response.data)
+                        //Si se obtine codigo 200, se envia a la lista de enviadas
+                        envio_info.data_enviada.push( elemento )
+                    })
+                    .catch(function (error) {
+                        //Caso contrario se reporta y se enviua a la lista de errores
+                    //console.log("Error al realizar petición", cantidad );
+                        envio_info.data_error.push( elemento )
+                    })
+            }
+        }
+    }
+    return
+}
+
+setInterval(async () => {
+    await procesar_envios()
+}, INTERVALO_ENVIO)
 
 httpServer.listen(port, () => console.log(`server listening on port ${port}`));
