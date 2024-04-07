cfe2283a1407975252371b16e94d309f554699f3
Luciano <lucho.2012.tandil@gmail.com>
2024-04-07 16:12:28 -0300

    Se quita registro en archivo, eso lo hace el server intermedio

--- modulos/mercadonaturaltandil/get_data.py
+++ modulos/mercadonaturaltandil/get_data.py
@@ -11,6 +11,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import socketio
+sio = socketio.SimpleClient()
+sio.connect('http://localhost:7777')
+
+sio.emit('cliente_conectado')
+if (not sio.receive()[1]["status"]):
+    print("Rechazado")
+    exit()
 BRANCH_ID = 94
 
 with open('categorias.json') as archivo_json:
@@ -20,8 +28,6 @@ with open("../config.json", "r") as archivo:
     config = json.load(archivo)
 
 fecha = datetime.datetime.now().strftime("%Y%m%d")
-
-listado_productos = []
 
 def procesar_resultados(res_consulta, categoria):
     soup = BeautifulSoup(res_consulta, 'html.parser')
@@ -46,9 +52,9 @@ def procesar_resultados(res_consulta, categoria):
                     "category": categorias[categoria]["category"],
                     "key": config["BACK_KEY"]
                 }
-        enviar_back = requests.post(config["URL_BACK"] + "/publico/productos/importar", json=producto)
-        print(enviar_back.json())
-        listado_productos.append(producto)
+        #enviar_back = requests.post(config["URL_BACK"] + "/publico/productos/importar", json=producto)
+        #print(enviar_back.json())
+        sio.emit('registrar_precio', producto)
         print(producto)
 
 def scroll_hasta_el_final(driver):
@@ -83,9 +89,3 @@ for categoria in categorias:
     
     scroll_hasta_el_final(driver)
     procesar_resultados(res_consulta, categoria)
-    
-
-    path = 'salida/productos_cat'+fecha+'.json'
-    with open(path, 'w') as file:
-        json.dump(listado_productos, file)
-        print(path,' actualizado')
