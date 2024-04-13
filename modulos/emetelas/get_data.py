7440cd5304523fea17d6de4649bc5718b933b851
Luciano <lucho.2012.tandil@gmail.com>
2024-04-13 19:29:18 -0300

    Se implementan funciones genericas

--- modulos/emetelas/get_data.py
+++ modulos/emetelas/get_data.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 import requests
 import datetime
-import json
-import socketio
+import sys
+
+sys.path.insert(1, "./modulos")
+from clientecoordinador import *
+cliente = ClienteCoordinador()
 
 BRANCH_ID = 83
 fecha = datetime.datetime.now().strftime("%Y%m%d")
 
-with open("../config.json", "r") as archivo:
-    config = json.load(archivo)
-    
 categorias = [
     {
         "name": "hogar-y-decoracion",
@@ -34,37 +34,27 @@ categorias = [
     }
 ]
 
+for categoria in categorias:
+    url = categoria["url"]
+    categoria = categoria["category"]
+    response = requests.get(url)
 
+    if response.json():
+        respuesta = response.json()
+        respuesta = respuesta["data"]
 
-with socketio.SimpleClient() as sio:
-    sio.connect('http://localhost:7777')
-
-    sio.emit('cliente_conectado')
-    if (not sio.receive()[1]["status"]):
-        print("Rechazado")
-        exit()
-
-    for categoria in categorias:
-        url = categoria["url"]
-        categoria = categoria["category"]
-        response = requests.get(url)
-
-        if response.json():
-            respuesta = response.json()
-            respuesta = respuesta["data"]
-
-            for prod in respuesta:
-                if prod["status"] == "available":
-                    producto = {
-                        "vendor_id": 58,
-                        "name": prod["name"],
-                        "price": prod["price"],
-                        "is_ext": prod["id"],
-                        "branch_id": BRANCH_ID,
-                        "category": categoria,
-                        "url": "https://emetelas.com.ar/"+prod["slug"],
-                        "key": config["BACK_KEY"]
-                    }
-                    sio.emit('registrar_precio', producto)
-                    print(producto)
-                    print("")
+        for prod in respuesta:
+            if prod["status"] == "available":
+                producto = {
+                    "vendor_id": 58,
+                    "name": prod["name"],
+                    "price": prod["price"],
+                    "is_ext": prod["id"],
+                    "branch_id": BRANCH_ID,
+                    "category": categoria,
+                    "url": "https://emetelas.com.ar/"+prod["slug"],
+                    "key": CONFIG["BACK_KEY"]
+                }
+                cliente.sio.emit('registrar_precio', producto)
+                print(producto)
+                print("")
