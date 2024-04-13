bf85f033695a0edcaebe7ac095241e2837fb3068
Luciano <lucho.2012.tandil@gmail.com>
2024-04-13 19:57:25 -0300

    Se implementan funciones genericasç

--- modulos/prestigio/get_data.py
+++ modulos/prestigio/get_data.py
@@ -1,51 +1,24 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 import json
 import requests
 from bs4 import BeautifulSoup
 import datetime
-import argparse
-import socketio
 
-sio = socketio.SimpleClient()
-sio.connect('http://localhost:7777')
+import sys
 
-sio.emit('cliente_conectado')
-if (not sio.receive()[1]["status"]):
-    print("Rechazado")
-    exit()
-
-with open('categorias.json') as archivo_json:
-    categorias = json.load(archivo_json)
-
-with open("../config.json", "r") as archivo:
-    config = json.load(archivo)
+sys.path.insert(1, "./modulos")
+from clientecoordinador import *
+cliente = ClienteCoordinador()
+from selenium_utils import *
 
 BRANCH_ID = 148
 BASE_URL = "https://www.prestigio.com.ar"
 
-options = webdriver.ChromeOptions()
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-#options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
+driver = get_driver()
 
 fecha = datetime.datetime.now().strftime("%Y%m%d")
-
-
 diccio_nam = {}
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("--categoria_inicio", type=str, help="Categoria desde la cual se procesan resultados")
-args = parser.parse_args()
-categoria_inicio = args.categoria_inicio
 
 def procesar_elementos( url, cat_id, categoria ):
     cantidad = 0
@@ -91,10 +64,10 @@ def procesar_elementos( url, cat_id, categoria ):
                         "is_ext": "",
                         "branch_id": BRANCH_ID,
                         "category": cat_id,
-                        "key": config["BACK_KEY"]
+                        "key": CONFIG["BACK_KEY"]
                     }
                 print(producto)
-                sio.emit('registrar_precio', producto)
+                cliente.sio.emit('registrar_precio', producto)
                 print("")
             except:
                 print("error al procesar")
@@ -105,22 +78,22 @@ def procesar_elementos( url, cat_id, categoria ):
     return cantidad
 
 procesar = True
-print(categoria_inicio)
+print(CATEGORIA_INICIO)
 
-if (categoria_inicio != None):
+if (CATEGORIA_INICIO != None):
     procesar = False
 
 total = 0
-for categoria in categorias:
-    url = categorias[categoria]['url']
+for categoria in CATEGORIAS:
+    url = CATEGORIAS[categoria]['url']
 
-    if (categoria == categoria_inicio):
-        print(categoria, categoria_inicio)
+    if (categoria == CATEGORIA_INICIO):
+        print(categoria, CATEGORIA_INICIO)
         procesar = True
         continue
 
     if (procesar == True):
-        total = total + procesar_elementos( url, categorias[categoria]["category"],  categoria )
+        total = total + procesar_elementos( url, CATEGORIAS[categoria]["category"],  categoria )
     else:
         print("ignorando categoria: ", categoria)
         continue
