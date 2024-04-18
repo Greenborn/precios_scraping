322fecbb79aaf93cc650daa41f672a5e932719f1
Luciano <lucho.2012.tandil@gmail.com>
2024-04-17 21:05:19 -0300

    Proceso que inicia los bots pasa como parametro la categoría inicial (en caso de que venga definida por el contenido del archivo runtime)

--- server_control_bots.js
+++ server_control_bots.js
@@ -10,16 +10,22 @@ let habilitados  = []
 let ejecutados   = []
 let por_ejecutar = []
 
-async function ejecutar_bot( item ){
-    console.log('Ejecutando: ', item)
+async function ejecutar_bot( item, estado_bot ){
+    console.log('Ejecutando: ', item, estado_bot)
+    let parametros = [
+        '--ruta_categorias', __dirname+'/modulos/'+item["d"]+'/categorias.json',
+        '--ruta_config', __dirname+'/modulos/config.json'
+    ]
+
+    if (estado_bot !== undefined){
+        parametros.push( '--categoria_inicio_id' )
+        parametros.push( estado_bot["category"] )
+    }
 
     let options = {
         mode: 'text',
         pythonOptions: ['-u'], // get print results in real-time
-        args: [
-            '--ruta_categorias', __dirname+'/modulos/'+item["d"]+'/categorias.json',
-            '--ruta_config', __dirname+'/modulos/config.json'
-        ]
+        args: parametros
     }
 
     PythonShell.run(__dirname+'/modulos/'+item["d"]+'/'+item["e"]+'.py', options, function (err, result){
@@ -37,16 +43,18 @@ fs.readFile("./modulos/habilitados.json", function(err, data) {
     por_ejecutar = [...habilitados]
 });
 
-
-setInterval( async ()=> {
-    os.cpuUsage(async function(v){
-        console.log( 'CPU Usage (%): ' + v )
-
-        if (v < USO_PROC_RUN && por_ejecutar.length > 0){
-            let a_ejecutar = por_ejecutar.pop()
-            ejecutados.push(a_ejecutar)
-
-            await ejecutar_bot( a_ejecutar )
-        }
-    })
-}, EJECUCION_TICK )
+exports.comenzar_ejecucion = ( estados_bots ) => {
+    console.log(estados_bots)
+    setInterval( async ()=> {
+        os.cpuUsage(async function(v){
+            console.log( 'CPU Usage (%): ' + v )
+    
+            if (v < USO_PROC_RUN && por_ejecutar.length > 0){
+                let a_ejecutar = por_ejecutar.pop()
+                ejecutados.push(a_ejecutar)
+    
+                await ejecutar_bot( a_ejecutar, estados_bots[a_ejecutar["branch_id"]] )
+            }
+        })
+    }, EJECUCION_TICK )
+}
